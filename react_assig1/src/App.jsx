057f68a3701a0f69363a8163027a1b69fa749986
AASTHA_2020 <modiaastha0@gmail.com>
2024-05-24 16:11:58 +0530

    completed

---
+++ react_assig1/src/App.jsx
@@ -0,0 +1,34 @@
+import "./App.css"
+
+function App() {
+
+
+  return (
+    <div className="main-container">
+    <div>
+      <h1>Topics Covered</h1> 
+      <p>The following is a list of all topics we cover in the MDN learning area</p>
+
+      </div>
+
+      <div>
+        <a href="">Getting started with the web</a>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique eos tempora dignissimos eligendi alias? Quaerat impedit necessitatibus vitae iure eaque.</p>
+
+      </div>
+      <div>
+        <a href="">HTML- Structuring the web </a>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique eos tempora dignissimos eligendi alias? Quaerat impedit necessitatibus vitae iure eaque.</p>
+        
+      </div>
+      <div>
+        <a href="">CSS- Styling the web</a>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique eos tempora dignissimos eligendi alias? Quaerat impedit necessitatibus vitae iure eaque.</p>
+        
+      </div>
+    
+      </div>
+  )
+}
+
+export default App
